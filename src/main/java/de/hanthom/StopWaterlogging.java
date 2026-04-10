66278d0f73ed84bc5d7a5fd28d77ebfa686b7622
hanth0m <129209706+hanth0m@users.noreply.github.com>
2026-04-10 19:04:15 +0200

    initial commit 1.20.5-1.21.4

---
+++ src/main/java/de/hanthom/StopWaterlogging.java
@@ -0,0 +1,17 @@
+package de.hanthom;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class StopWaterlogging implements ModInitializer {
+	public static final String MOD_ID = "stop-waterlogging";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		//nothing here yet
+	}
+}
---
+++ src/main/java/de/hanthom/mixin/BucketItemMixin.java
@@ -0,0 +1,40 @@
+package de.hanthom.mixin;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.BucketItem;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.phys.BlockHitResult;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
+@Mixin(BucketItem.class)
+public abstract class BucketItemMixin implements BucketItemAccessorMixin {
+	@ModifyArgs(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/BucketItem;emptyContents(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/phys/BlockHitResult;)Z"))
+	private void cancelWaterLogs(Args args) {
+		LivingEntity user = args.get(0);
+		Level world = args.get(1);
+		BlockPos pos = args.get(2);
+		BlockHitResult hitResult = args.get(3);
+		if (world.getBlockState(pos).hasProperty(BlockStateProperties.WATERLOGGED) && this.getFluid() == Fluids.WATER) {
+			if (user.isShiftKeyDown()) {
+				args.set(2, pos.relative(hitResult.getDirection()));
+			}
+		} else if (world.getBlockState(hitResult.getBlockPos()).is(BlockTags.SMALL_FLOWERS)
+				|| world.getBlockState(hitResult.getBlockPos()).is(BlockTags.SAPLINGS)
+				|| world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.SHORT_GRASS //|| world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.BUSH
+				|| world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.DEAD_BUSH //|| world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.LEAF_LITTER
+				|| world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.FERN
+				|| world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.PINK_PETALS
+				//|| world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.WILDFLOWERS
+		) {
+			args.set(2, hitResult.getBlockPos());
+		}
+	}
+}
