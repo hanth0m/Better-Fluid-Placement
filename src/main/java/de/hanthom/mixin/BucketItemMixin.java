2d1d0bf64b0827cf5d4abb517fcc6f50c13df607
hanth0m <129209706+hanth0m@users.noreply.github.com>
2026-04-11 13:50:15 +0200

    backported to legacy fabric & MC 1.13.2

--- src/main/java/de/hanthom/mixin/BucketItemMixin.java
+++ src/main/java/de/hanthom/mixin/BucketItemMixin.java
@@ -1,15 +1,15 @@
 package de.hanthom.mixin;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.tags.BlockTags;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.BucketItem;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.material.Fluid;
-import net.minecraft.world.level.material.Fluids;
-import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.BucketItem;
+import net.minecraft.states.property.Properties;
+import net.minecraft.tag.BlockTags;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -21,20 +21,30 @@ import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 public abstract class BucketItemMixin {
 	@Shadow
 	@Final
-	private Fluid content;
+	private Fluid field_17157;
 
-	@ModifyArgs(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/BucketItem;emptyBucket(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/phys/BlockHitResult;)Z"))
+	@ModifyArgs(method = "method_13649", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/BucketItem;method_16028(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/hit/BlockHitResult;)Z"))
 	private void cancelWaterLogs(Args args) {
 		LivingEntity user = args.get(0);
-		Level world = args.get(1);
+		World world = args.get(1);
 		BlockPos pos = args.get(2);
 		BlockHitResult hitResult = args.get(3);
-		if (world.getBlockState(pos).hasProperty(BlockStateProperties.WATERLOGGED) && this.content == Fluids.WATER) {
-			if (user.isShiftKeyDown()) {
-				args.set(2, pos.relative(hitResult.getDirection()));
+		if (world.getBlockState(pos).method_16933(Properties.WATERLOGGED) && this.field_17157 == Fluids.WATER) {
+			if (user.isSneaking()) {
+				args.set(2, pos.offset(hitResult.direction));
 			}
-		} else if (!user.isShiftKeyDown() && (world.getBlockState(hitResult.getBlockPos()).is(BlockTags.SMALL_FLOWERS)
-				|| world.getBlockState(hitResult.getBlockPos()).is(BlockTags.SAPLINGS)
+		} else if (!user.isSneaking() && (
+				world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.POPPY
+				|| world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.DANDELION
+				|| world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.AZURE_BLUET
+				|| world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.ORANGE_TULIP
+				|| world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.PINK_TULIP
+				|| world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.RED_TULIP
+				|| world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.WHITE_TULIP
+				|| world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.BLUE_ORCHID
+				|| world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.ALLIUM
+				|| world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.OXEYE_DAISY
+				|| world.getBlockState(hitResult.getBlockPos()).isIn(BlockTags.SAPLINGS)
 				|| world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.GRASS
 				|| world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.DEAD_BUSH
 				|| world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.FERN
