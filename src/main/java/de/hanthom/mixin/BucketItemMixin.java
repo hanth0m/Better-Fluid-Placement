78c02ccfffdc2cc4805741ba3b6755f488cc3a5b
hanth0m <129209706+hanth0m@users.noreply.github.com>
2026-04-11 03:12:09 +0200

    updated to 1.21.5-1.21.11

--- src/main/java/de/hanthom/mixin/BucketItemMixin.java
+++ src/main/java/de/hanthom/mixin/BucketItemMixin.java
@@ -16,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(BucketItem.class)
 public abstract class BucketItemMixin implements BucketItemAccessorMixin {
-	@ModifyArgs(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/BucketItem;emptyContents(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/phys/BlockHitResult;)Z"))
+	@ModifyArgs(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/BucketItem;emptyContents(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/phys/BlockHitResult;)Z"))
 	private void cancelWaterLogs(Args args) {
 		LivingEntity user = args.get(0);
 		Level world = args.get(1);
@@ -28,11 +28,13 @@ public abstract class BucketItemMixin implements BucketItemAccessorMixin {
 			}
 		} else if (!user.isShiftKeyDown() && (world.getBlockState(hitResult.getBlockPos()).is(BlockTags.SMALL_FLOWERS)
 				|| world.getBlockState(hitResult.getBlockPos()).is(BlockTags.SAPLINGS)
-				|| world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.GRASS //|| world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.BUSH
-				|| world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.DEAD_BUSH //|| world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.LEAF_LITTER
+				|| world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.SHORT_GRASS || world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.BUSH
+				|| world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.DEAD_BUSH || world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.LEAF_LITTER
 				|| world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.FERN
 				|| world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.PINK_PETALS
-				//|| world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.WILDFLOWERS
+				|| world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.WILDFLOWERS
+				|| world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.SHORT_DRY_GRASS
+				|| world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.FIREFLY_BUSH
 		)) {
 			args.set(2, hitResult.getBlockPos());
 		}
